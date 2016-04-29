ebbc6bbe4cf579dad716dbb643b44e7496490f2b
theoboldt <e.s.t.m52@gmx.de>
2016-04-29 20:17:56 +0200

    Added simplest error handling if sensor unavailable

--- sensor.py
+++ sensor.py
@@ -14,15 +14,18 @@ def path():
 SENSOR_PATH = path()
 
 
-# Temperatur ermitteln und Wert value zurckgeben
 def read():
-    a = open(SENSOR_PATH, 'r')  # Ermittelten Sensor lesen
-    value_raw = a.readline()
-
-    if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", value_raw):  # Erste Zeile auf "YES" am Ende uberprufen
+    try:
+        a = open(SENSOR_PATH, 'r')  # Ermittelten Sensor lesen
         value_raw = a.readline()
-        m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", value_raw)  # Wert aus zweiter Zeile prufen
-    if m:
-        value = float(m.group(2)) / 1000.0
-    a.close()
-    return value  # Ruckgabewert "value" definieren
+
+        if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", value_raw):  # Erste Zeile auf "YES" am Ende uberprufen
+            value_raw = a.readline()
+            m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", value_raw)  # Wert aus zweiter Zeile prufen
+        if m:
+            value = float(m.group(2)) / 1000.0
+        a.close()
+        return value  # Ruckgabewert "value" definieren
+
+    except UnboundLocalError:
+        return 9999
